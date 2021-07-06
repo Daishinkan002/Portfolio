c7d986ed7f261436673fbd5ecbd43a7a78e6777c
daishinkan002 <shantanutripathi002@gmail.com>
2021-07-06 23:28:02 +0530

    added transition in navbar

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
-  });
-  
+  })
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    if (scroll > 700) {
+      $(".nav-wrapper").css("background" , "black");
+    }
+
+    else{
+      $(".nav-wrapper").css("background" , "transparent", "transition", "background-color 200ms linear");  	
+    }
+  })
+});
