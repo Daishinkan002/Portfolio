f1d27571accb996783b09ef2f2f71c19f492a86d
daishinkan002 <shantanutripathi002@gmail.com>
2024-12-28 10:52:20 +0530

    Loaded dom content before calling function

--- js/navsticky_onscroll.js
+++ js/navsticky_onscroll.js
@@ -1,18 +1,26 @@
-window.onscroll = function() {
-    stickyNavbar();
-};
+document.addEventListener("DOMContentLoaded", function() {
+    window.onscroll = function() {
+        stickyNavbar();
+    };
 
-function stickyNavbar() {
-    var navbar = document.querySelector("nav");
-    var header = document.querySelector("header");
+    function stickyNavbar() {
+        var navbar = document.querySelector("nav");
+        var header = document.querySelector("header");
 
-    // Get the offset position of the header
-    var headerHeight = header.offsetHeight;
+        // Check if navbar and header exist to avoid errors if not found
+        if (!navbar || !header) {
+            console.error("Navbar or header element not found!");
+            return;
+        }
 
-    // If the page is scrolled past the header, add the sticky class to the navbar
-    if (window.pageYOffset > headerHeight) {
-        navbar.classList.add("sticky");
-    } else {
-        navbar.classList.remove("sticky");
+        // Get the offset position of the header
+        var headerHeight = header.offsetHeight;
+
+        // If the page is scrolled past the header, add the sticky class to the navbar
+        if (window.pageYOffset > headerHeight) {
+            navbar.classList.add("sticky");
+        } else {
+            navbar.classList.remove("sticky");
+        }
     }
-}
+});
